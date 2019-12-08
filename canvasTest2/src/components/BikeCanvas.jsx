0a8c7531ca954ca79a139104cd75b3d6332b3d5f
Ethan Samuels-Ellingson <ethansamuelsellingson@gmail.com>
2019-12-07 23:09:25 -0800

    add custom svg icons to src/assets/img

--- canvasTest2/src/components/BikeCanvas.jsx
+++ canvasTest2/src/components/BikeCanvas.jsx
@@ -14,6 +14,7 @@ class BikeCanvas extends React.Component {
     }
 
     render () {
+        
         var canvas = this.canvas;
         var img = null;
         var files = this.fileInput;
@@ -27,10 +28,8 @@ class BikeCanvas extends React.Component {
         }
 
         function drawImage(){
-            console.log('draw running');
-
-            console.log(img);
-            canvas.current.width = 800;
+            var windowWidth = window.innerWidth;
+            canvas.current.width = windowWidth * 0.8;
             console.log('ratio', canvas.current.width * (img.height / img.width) )
             canvas.current.height = canvas.current.width * (img.height / img.width);
             var ctx = canvas.current.getContext('2d');
@@ -61,18 +60,31 @@ class BikeCanvas extends React.Component {
             ctx.stroke();
         }
 
+        var pathStyle = {            
+            stroke: '#e08900',
+            strokeWidth: 15,
+            strokeLinecap: 'round',
+            strokeLinejoin: 'round',
+            strokeOpacity: 1,
+            strokeMiterlimit: 4,
+            fillOpacity: 1
+        }
+
         return (
             <div className='canvas'>          
                 <StyleSelection/>
+                
                 <input className='fileInput' type='file' ref={this.fileInput} onChange={onImageLoad} /> 
-                <div className='jkl'>
-                    <canvas ref={this.canvas} width='0' height='0' onClick={canvasClick.bind(this)} />
-                </div>
-               
+                <canvas ref={this.canvas} width='0' height='0' onClick={canvasClick.bind(this)} />
+                <input type='number'></input>
                 {/* <button onClick={setSvgActive}>Submit</button> */}
+                <svg viewBox="0 0 409.6 405.76">
+                    <path d="M682.8,396.06c50.72,0,91.84-48.13,91.84-107.49,0-82.33-41.12-107.49-91.84-107.49S591,206.24,591,288.57c0,59.36,41.12,107.49,91.84,107.49Zm0,0" transform="translate(-478 -181.08)" />
+                    <path d="M885.6,554.28,839.27,449.9a23.3,23.3,0,0,0-10.48-11.15l-71.91-37.43a4.66,4.66,0,0,0-4.93.41,113.41,113.41,0,0,1-138.3,0,4.67,4.67,0,0,0-4.94-.41l-71.9,37.43a23.24,23.24,0,0,0-10.47,11.15L480,554.28a23.16,23.16,0,0,0,21.18,32.56H864.42a23.17,23.17,0,0,0,21.18-32.56Zm0,0" transform="translate(-478 -181.08)" />
+                </svg>
+
             </div>
         );
     }
 }
-
 export default BikeCanvas;
