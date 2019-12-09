929771377d1e114d2b33f2b0364014cac1e0f491
Ethan Samuels-Ellingson <ethansamuelsellingson@gmail.com>
2019-12-08 23:18:53 -0800

    add bag selection svg

--- canvasTest2/src/components/BikeCanvas.jsx
+++ canvasTest2/src/components/BikeCanvas.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import StyleSelection from './StyleSelection';
 
-
 class BikeCanvas extends React.Component {
     constructor(props) {
         super(props);
@@ -78,11 +77,6 @@ class BikeCanvas extends React.Component {
                 <canvas ref={this.canvas} width='0' height='0' onClick={canvasClick.bind(this)} />
                 <input type='number'></input>
                 {/* <button onClick={setSvgActive}>Submit</button> */}
-                <svg viewBox="0 0 409.6 405.76">
-                    <path d="M682.8,396.06c50.72,0,91.84-48.13,91.84-107.49,0-82.33-41.12-107.49-91.84-107.49S591,206.24,591,288.57c0,59.36,41.12,107.49,91.84,107.49Zm0,0" transform="translate(-478 -181.08)" />
-                    <path d="M885.6,554.28,839.27,449.9a23.3,23.3,0,0,0-10.48-11.15l-71.91-37.43a4.66,4.66,0,0,0-4.93.41,113.41,113.41,0,0,1-138.3,0,4.67,4.67,0,0,0-4.94-.41l-71.9,37.43a23.24,23.24,0,0,0-10.47,11.15L480,554.28a23.16,23.16,0,0,0,21.18,32.56H864.42a23.17,23.17,0,0,0,21.18-32.56Zm0,0" transform="translate(-478 -181.08)" />
-                </svg>
-
             </div>
         );
     }
--- canvasTest2/src/components/StyleSelection.jsx
+++ canvasTest2/src/components/StyleSelection.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import TopTube from '../assets/img/Toptube';
+import Full from '../assets/img/Full';
+import Front from '../assets/img/Front';
+
 function Header() {
     return (
         <div className='styleSelection grid-container'>
             <div className='grid-item'>
                 <h4>Top Tube</h4>
+                <TopTube className='bagIcon'/>
             </div>
             <div className='grid-item'>
                 <h4>Front</h4>
+                <Front className='bagIcon' />
             </div>
             <div className='grid-item'>
                 <h4>Full</h4>
+                <Full className='bagIcon' />
             </div>
         </div>
     )
