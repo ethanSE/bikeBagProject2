ca54280ccb47e2d0654660b66bf900db9df8ba7e
Student <example@example.com>
2019-12-11 11:31:21 -0800

    add jest tests for reducers

---
+++ src/__tests__/reducers/coordsReducer.tes.js
@@ -0,0 +1,21 @@
+import coordsReducer from '../../reducers/coordsReducer';
+
+describe('coordsReducer', () => {
+    test('Should return default state if no action type is recognized', () => {
+      expect(coordsReducer({}, { type: null })).toEqual({})
+    });
+  
+  
+    let action;
+    const coords = [0,0];
+  
+    test('Should return new coords when setcoords is sent', () => {
+      action = {
+        type: 'SET_COORDINATES',
+        coords: coords
+      };
+      expect(coordsReducer({}, action)).toEqual(
+        [0,0]
+      );
+    });
+  });
