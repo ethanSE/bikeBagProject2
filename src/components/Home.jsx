8c74535f31975065f3656140e23e5acf2813de48
Ethan Samuels-Ellingson <ethansamuelsellingson@gmail.com>
2020-01-12 14:29:35 -0800

    add some generic content to home/how it works page. practice css grid and naming lines

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
+import { setActiveMainComponent } from './../actions';
 import { connect } from 'react-redux';
+import '../styles/Home.css';
 
 function Home(props) {
     if (props.activeMainComponent === 'home') {
         return (
-            <div className='grid-container-about'>
-                <div className='heroDiv'>
+            <div className='home'>
+                <h1>How it works</h1>
+                <button className='button' onClick={() => { props.dispatch(setActiveMainComponent('custom')) }}> create custom bag</button>
+                <div className='image1'/>
+                <div className='image2'/>
+                <div className='image3'/>
+                <div className='text text1'>
+                    <h3>Step 1</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis sequi optio asperiores sint aliquid in rem iure suscipit! Odit eum hic incidunt. Quidem corrupti expedita, officia doloremque praesentium maiores minima.</p>
                 </div>
-
-                <div className='inlinePhoto1 grid-item-about'>
+                <div className='text text2'>
+                    <h3>Step 2</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis cumque repellendus quidem? Facere, commodi unde beatae numquam, animi ratione consequuntur doloremque quia labore voluptates eius impedit officiis exercitationem doloribus sint?</p>
                 </div>
-
-                <div className='textField1 grid-item-about'>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, totam pariatur dignissimos voluptate eius cumque et excepturi maiores! Sit omnis accusamus corporis unde assumenda ab nihil alias eligendi, itaque autem?</p>
-                </div>
-
-                <div className='inlinePhoto2 grid-item-about'>
-                </div>
-
-                <div className='textField2 grid-item-about'>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, tempore reiciendis minus praesentium omnis recusandae earum a velit ducimus illum, aliquid facere veritatis, odit porro qui doloremque cum ipsa corporis!</p>
+                <div className='text text3'>
+                    <h3>Step 3</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis cumque repellendus quidem? Facere, commodi unde beatae numquam, animi ratione consequuntur doloremque quia labore voluptates eius impedit officiis exercitationem doloribus sint?</p>
                 </div>
             </div>
+
         )
     } else {
         return null;
