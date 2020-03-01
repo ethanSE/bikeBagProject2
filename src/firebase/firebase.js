75f66b8ae3de1958430fd86e83f1006cf14f37be
Ethan Samuels-Ellingson <ethansamuelsellingson@gmail.com>
2020-03-01 11:42:16 -0800

    removeoldstatemanagementandfirebase

--- src/firebase/firebase.js
+++
@@ -1,5 +0,0 @@
-import firebase from 'firebase';
-import constants from './../constants';
-const { firebaseConfig } = constants;
-
-export const myFirebase = firebase.initializeApp(firebaseConfig);
--- src/reducers/imageReducer.js
+++
@@ -1,9 +0,0 @@
-const imageReducer = (state = '', action) => {
-    switch (action.type) {
-        case 'SET_IMAGE':
-            return action.image;
-        default:
-            return state;   
-    };
-}
-export default imageReducer;
