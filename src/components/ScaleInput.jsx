285e059b2801c7f2b0ec1bbde69983700b4a2968
Ethan Samuels-Ellingson <ethansamuelsellingson@gmail.com>
2020-03-08 23:57:37 -0700

    more styling refactor and scale input refactor

--- src/components/ScaleInput.jsx
+++ src/components/ScaleInput.jsx
@@ -1,6 +1,7 @@
-import React, { useRef, useContext, useEffect, useState } from 'react';
+import React, { useRef, useContext, useEffect, useState, useLayoutEffect } from 'react';
 import { CustomSpecContext } from '../customSpecContext';
 import styles from '../styles/ScaleInput.module.css'
+import { Button } from 'react-bootstrap';
 
 function debounce(fn, ms) {
     let timer
@@ -22,56 +23,39 @@ const ScaleInput = () => {
     let scaleInputRef = useRef();
     let scaleInputDivRef = useRef();
 
+
     useEffect(() => {
         window.addEventListener('resize', debouncedHandleResize)
         return () => {
             window.removeEventListener('resize', debouncedHandleResize)
         };
     })
-    const debouncedHandleResize = debounce(() => setWindowWidth(window.innerWidth), 10)
+    const debouncedHandleResize = debounce(() => setWindowWidth(window.innerWidth), 100)
 
-
-    // useEffect(() => {
-    //     topTubePoints.forEach((point) => drawCircle(point[0], point[1]));
-    // }, [topTubePoints, windowWidth]);
-
-    let ctx;
-
-    if (customSpecState.image) {
-        var image = new Image();
-        image.onload = () => {
-            canvasScaleRef.current.width = scaleInputDivRef.current.clientWidth;
-            canvasScaleRef.current.height = canvasScaleRef.current.width * (image.height / image.width);
-            ctx = canvasScaleRef.current.getContext('2d');
-            ctx.drawImage(image, 0, 0, canvasScaleRef.current.width, canvasScaleRef.current.height);
+    useEffect(() => {
+        if (customSpecState.image) {
+            var image = new Image();
+            image.onload = () => {
+                canvasScaleRef.current.width = scaleInputDivRef.current.clientWidth; //* ratio added
+                canvasScaleRef.current.height = canvasScaleRef.current.width * (image.height / image.width); //* ratio added
+                let ctx = canvasScaleRef.current.getContext('2d');
+                ctx.drawImage(image, 0, 0, canvasScaleRef.current.width, canvasScaleRef.current.height);
+            }
+            image.src = customSpecState.image;
         }
-        image.src = customSpecState.image;
-    }
+    }, [windowWidth, topTubePoints])
 
-    const canvasScaleClick = (evt) => {
-        let rect = canvasScaleRef.current.getBoundingClientRect();
-        let x = (evt.clientX - rect.left);
-        let y = (evt.clientY - rect.top);
-        console.log(x, y)
-        if (topTubePoints.length < 2) {
-            console.log('hi')
-            
-            // drawCircle(x, y); have useEffect do drawing?
-            setTopTubePoints([...topTubePoints, [x, y]]);
-            console.log(topTubePoints)
+    useEffect(() => {
+        if (topTubePoints) {
+            topTubePoints.forEach(point => drawCircle(point[0] * canvasScaleRef.current.width, point[1] * canvasScaleRef.current.height ))
         }
-    }
+    }, [topTubePoints])
 
-    const setPixelToInchScale = (event) => {
-        event.preventDefault();
-        if (topTubePoints.length === 2) {
-            let distance = Math.hypot(topTubePoints[0][0] - topTubePoints[1][0], topTubePoints[0][1] - topTubePoints[1][1])
-            let scale = (distance / scaleInputRef.value);
-        }
-    }
 
     const drawCircle = (x, y) => {
-        var ctx = canvasScaleRef.current.getContext('2d');
+        console.log('draw circle running')
+        console.log(x, y)
+        let ctx = canvasScaleRef.current.getContext('2d');
         ctx.strokeStyle = "#FF0000";
         ctx.lineWidth = 3;
         ctx.beginPath();
@@ -79,23 +63,52 @@ const ScaleInput = () => {
         ctx.stroke();
     }
 
+    const canvasScaleClick = (evt) => {
+        if ( !topTubePoints) {
+            let rect = canvasScaleRef.current.getBoundingClientRect();
+            let x = (evt.clientX - rect.left);
+            let y = (evt.clientY - rect.top);
+            let xRatio = x / canvasScaleRef.current.width;
+            let yRatio = y / canvasScaleRef.current.height;
+            setTopTubePoints([xRatio, yRatio]);
+
+
+
+
+        } else if (topTubePoints.length < 2 ){
+            let rect = canvasScaleRef.current.getBoundingClientRect();
+            let x = (evt.clientX - rect.left);
+            let y = (evt.clientY - rect.top);
+            let xRatio = x / canvasScaleRef.current.width;
+            let yRatio = y / canvasScaleRef.current.height;
+            setTopTubePoints([...topTubePoints, [xRatio, yRatio]]); // might not work on a null
+        }
+    }
 
     const resetCoordinates = () => {
-        setTopTubePoints([]);
-        ctx.drawImage(image, 0, 0, canvasScaleRef.current.width, canvasScaleRef.current.height);
+        console.log('reset')
+        setTopTubePoints(null);
+    }
+
+    const setPixelToInchScale = (event) => {
+        event.preventDefault();
+        // if (topTubePoints.length === 2) {
+        //     let distance = Math.hypot(topTubePoints[0][0] - topTubePoints[1][0], topTubePoints[0][1] - topTubePoints[1][1])
+        //     let scale = (distance / scaleInputRef.value);
+        // }
     }
 
     switch (customSpecUIState.scale) {
         case 'active':
             return (
-                <div className='scaleInput customActive' ref={scaleInputDivRef}>
+                <div className={styles.scaleInput} ref={scaleInputDivRef}>
                     <h3>Scale</h3>
-                    <form onSubmit={(event) => setPixelToInchScale(event)} className='scaleInputForm'>
+                    <form onSubmit={(event) => setPixelToInchScale(event)} className={styles.scaleInputForm}>
                         <input ref={(input) => { scaleInputRef = input }} placeholder='Top Tube Length in inches' type='number' />
-                        <button className='button' type='submit'>Submit</button>
-                        <button className='button' onClick={resetCoordinates}>Reset</button>
+                        <button className={styles.button} type='submit'>Submit</button>
+                        <button className={styles.button} onClick={resetCoordinates}>Reset</button>
                     </form>
-                    <canvas className='scaleInputCanvas' ref={canvasScaleRef} width='0' height='0' onClick={canvasScaleClick} />
+                    <canvas ref={canvasScaleRef} width='0' height='0' onClick={canvasScaleClick} />
                 </div>
             )
         case 'minimized':
