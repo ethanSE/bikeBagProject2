c256cdf41587d8c17d480882aac309d996cd39df
Ethan Samuels-Ellingson <ethansamuelsellingson@gmail.com>
2020-03-15 16:11:04 -0700

    Add aws auth and Google Oauth

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,20 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { ModeContext } from '../modeContext';
 import styles from '../styles/Header.module.css';
+import Amplify from 'aws-amplify';
+import config from '../aws-exports';
+import { Auth, Hub } from 'aws-amplify';
+
+Amplify.configure(config)
 
 const Header = () => {
     const { setActiveMainComponent } = useContext(ModeContext);
-    let account = <h3  onClick={() => console.log()}> Sign In </h3>
 
     return (
         <div className={styles.header}>
             <h1 style={styles.headerTitle}>Bike Frame Bag Designer</h1>
             <div className={styles.links}>
-                {account}
+                <SignInOutComponent />
                 <h3 onClick={() => setActiveMainComponent('customSpec')}> Custom </h3>
                 <h3 onClick={() => setActiveMainComponent('home')}> Home </h3>
             </div>
@@ -19,3 +23,41 @@ const Header = () => {
 }
 
 export default Header;
+
+const SignInOutComponent = () => {
+    const [ user, setUser] = useState(null)
+
+    useEffect(() => {
+        Hub.listen("auth", ({ payload: { event, data } }) => {
+            if (event === "signOut") {
+                setUser(null);
+            } else if (event === 'signIn') {
+                setUser(Auth.currentAuthenticatedUser());
+            }
+        });
+    });
+
+    if (user) {
+        console.log('there mightbe a user')
+        return (<h3 onClick={() => Auth.signOut()}> Sign Out </h3>)
+    } else {
+        console.log('there might not be a user')
+        return (
+            <h3 onClick={() => Auth.federatedSignIn()}> Sign In </h3>
+        )
+    }
+}
+
+
+// const user = Auth.currentAuthenticatedUser();
+
+// console.log('user on startup', user)
+
+// const account = () => {
+//     return Auth.currentAuthenticatedUser().ID ? (
+//         
+//     ) :
+//         (
+//             <h3 onClick={() => Auth.federatedSignIn({ provider: 'Google' })}> Sign In </h3>
+//         )
+// }
